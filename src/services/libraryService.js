7a279f21f98b4e8de249d1a93fa5eee5846393a7
Murillo <murillo.ags@gmail.com>
2026-06-17 01:16:54 -0300

    minimal changes

--- src/services/libraryService.js
+++ src/services/libraryService.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:3000";
-// const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getLibraries() {
   const response = await fetch(`${API_URL}/libraries`);
