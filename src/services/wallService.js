8ae215e715f8126c75e5de8005f566c90f9c545e
Murillo Anthony <102196370+Murillo-505@users.noreply.github.com>
2026-06-25 20:58:02 -0300

    Update wallService.js

--- src/services/wallService.js
+++ src/services/wallService.js
@@ -1,5 +1,9 @@
 // const API_URL = "http://localhost:3000";
 const API_URL = import.meta.env.VITE_API_URL;
+console.log(
+  'API_URL:',
+  import.meta.env.VITE_API_URL
+)
 
 export async function getWalls() {
   const response = await fetch(`${API_URL}/walls`);
